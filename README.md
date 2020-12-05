8715a6cdda652e56c42f41650371842d9fde1003
Rodrigo Patricio <rpatriciocarvalho@gmail.com>
2020-12-05 20:07:40 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,5 +1,56 @@
 # serverless-challenge
-Build a serverless architecture for image analysis 
+Construir uma arquitetura serverless para análise de imagens.
 
 ![Screenshot](Architecture.png)
 
+Esse projeto utiliza as seguintes tecnologias:
+
+ - Python
+ - Amazon S3
+ - AWS Lambda
+ - Amazon DynamoDB
+ - Amazon API Gateway
+ - Serverless Framework
+
+## Funções
+
+O projeto possui 4 funções.
+
+### extractMetadata
+Responsável por extrair o nome, tamanho e tipo da imagem carregada e registrar no banco de dados. Não é acessada por método GET.
+
+Quando uma imagem (ou mais) no formato .jpg, .jpeg, .png ou .gif é carregada na pasta `uploads` do bucket na Amazon S3 o AWS Lambda dispara a função `extractMetadata`. Essa função  recebe no parâmetro `event` a chave do objeto e o seu tamanho. 
+
+O formato do a imagem é extraída da chave do objeto, onde todas as letras são transformadas em minúsculas para evitar redundâncias como `.jpg` e `.JPG`.
+
+A chave do objeto, seu tamanho e tipo são salvos na tabela `serverless-challenge-dev`.
+
+> **Issue**
+> Falta implementar a análise da largura e altura da imagem.
+
+### getMetadata
+Recebe como parâmetro o nome da imagem `{s3objectkey}` e retorna os metadados registrados no banco de dados.
+
+    GET <url>/images/{s3objectkey}
+
+No arquivo `serverless.yml` foi implementado um evento para cada formato de arquivo.
+
+> **Issue**
+> No banco de dados há o prefixo "uploads/" antes do nome da imagem. Porém ainda não consegui implementar uma chamada do tipo `uploads/arquivo.jpg`. No momento basta informar o nome do arquivo, sem o prefixo.
+
+### infoImages
+Retorna do banco de dados o arquivo de maior e menor tamanho, os formatos de imagens registradas e a quantidade de imagens em cada formato.
+
+Não recebe nenhum parâmetro.
+
+        GET <url>/infoimages
+
+### getImage
+Recebe como parâmetro o nome do arquivo e retorna um link temporário para download. Similar a `getMetadata`, aqui também deve-se informar apenas o nome do arquivo na url, sem o prefixo `uploads/`.
+
+    GET <url>/get/{s3objectkey}
+
+No arquivo `serverless.yml` foi acrescentado a função correspondente.
+
+> **Issue**
+> O link temporário para download está retornando acesso negado. Acredito que seja alguma permissão no meu usuário IAM para que possa dar acesso a essa função.
--- handler.py
+++ handler.py
@@ -1,11 +1,104 @@
 # coding=utf-8
 
 import boto3
+import botocore
+from urllib.parse import unquote_plus
+import decimalencoder
+import json
+from collections import Counter
 
 dynamodb = boto3.resource("dynamodb")
+s3 = boto3.resource('s3')
 
 def extractMetadata(event, context):
-    print("Hello World")
+
+    table = dynamodb.Table("serverless-challenge-dev")
+    for record in event["Records"]:
+        #bucket = record['s3']['bucket']['name']
+        key = unquote_plus(record['s3']['object']['key'])
+        size = record['s3']['object']['size']      
+        
+        # gambiarra =(
+        
+        type_file = key.split('.')
+        type_file = str(type_file[-1]).lower()
+        
+        # fim da gambiarra =)
+
+        response = table.put_item(
+            Item={
+                's3objectkey': key,
+                'size': size,
+                "type": type_file
+            }
+        )
+    
+    return response
+
 
 def getMetadata(event, context):
-    print("Hello World")
+    
+    s3objectkey = event["pathParameters"]["s3objectkey"]
+    
+    table = dynamodb.Table("serverless-challenge-dev")
+    prefix = "uploads/"
+
+    result = table.get_item(
+        Key={
+            "s3objectkey": prefix + s3objectkey
+        }
+    )
+  
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result['Item'],
+                          cls=decimalencoder.DecimalEncoder)
+    } 
+
+    return response
+
+
+def getImage(event, context):
+
+    s3objectkey = event["pathParameters"]["s3objectkey"]
+    prefix = "uploads/"
+    bucket = "meusarquivosdodesafio"
+
+    s3_client = boto3.client('s3')
+    url = s3_client.generate_presigned_url('get_object', 
+                Params = {"Bucket": bucket, "Key": prefix + s3objectkey}, ExpiresIn=100)
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps({"URL": url})
+    }
+    
+    return response
+
+
+def infoImages(event, context):
+
+    table = dynamodb.Table("serverless-challenge-dev")
+
+    result = table.scan()
+    result = result['Items']
+
+    maior = max(result, key=lambda x:x['size'])
+    menor = min(result, key=lambda x:x['size'])
+    tipos_geral = list(d["type"] for d in result)
+    tipos_numero = Counter(tipos_geral)
+
+    resposta = {
+        "maiorTamanho": maior,
+        "menorTamanho": menor,
+        "formatos": list(set(tipos_geral)),
+        "quantidade": dict(tipos_numero)
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(resposta,
+                          cls=decimalencoder.DecimalEncoder)
+        }
+
+    return response
